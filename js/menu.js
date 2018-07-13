ae62b684ca88842763c6ec31c109519a19c5883a
Monika Nizinska <monika.nizinska@sparking.pro>
2018-07-13 12:42:39 +0200

    create responsive menu

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,30 @@
-$(".scroll").each(function() {  
-    $($(this)).click(function() { 
+$(document).ready(function () {
+    $("#nav-icon").click(function () {
+        $("#main-nav").toggleClass("nav-visible");
+
+    });
+});
+$(document).ready(function () {
+    $("#main-nav").click(function () {
+        $("#nav-icon").removeClass("nav-visible");
+    });
+});
+
+$(window).resize(function () {
+    if ($(document).width() > 992 && $("#main-nav").hasClass('nav-visible') && $('.collapse').hasClass('show')) {
+        $("#main-nav").removeClass("nav-visible");
+        $(".collapse").removeClass("show");
+
+    }
+});
+
+console.log($(window).width());
+
+
+$(".scroll").each(function () {
+    $($(this)).click(function () {
         $("html, body").animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 1500);
     });
-  });
-  
-//   $(function(){
-      
-//       let mainNav = $('#main-nav');
-//      $(window).scroll(function(){
-         
-//          if (window.scrollY > ($('#main-header').height()/10)) {
-//             $("#main-nav").css("background-color", "rgba(37, 0, 126, 0.5)");
-//             $('#logo').css("height", "30px");
-//          }
-         
-//          if (window.scrollY < ($('#main-header').height()/10)) {
-//             $("#main-nav").css("background-color", "transparent");
-//             $('#logo').css("height", "50px");
-//          }
-         
-//      });
-      
-//   });
-  
+});
